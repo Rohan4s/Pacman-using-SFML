bdab1b110915c285b75fb58c4456378332476ce7
Rohan4s <rohan22@student.sust.edu>
2021-05-01 16:00:35 +0600

    new comments

--- main.cpp
+++ main.cpp
@@ -8,15 +8,15 @@
 
 
 
-#define gridSize 15
-#define mapSize 40+1
+#define cellSize 15   // each cell is (15*15) pixels
+#define mapSize 40+1  // a total of 41 rows and columns
 
 
-bool isReachable(bool grid[][41], char direction, int currentPositionX, int currentPositionY ) {
+bool isReachable(bool grid[][41], char direction, int currentPositionX, int currentPositionY ) {  // Returns true if the 4 adjacent cells dont have walls
 	
 	int x, y;
-	x = currentPositionX / gridSize;
-	y = currentPositionY / gridSize;
+	x = currentPositionX / cellSize;
+	y = currentPositionY / cellSize;
 
 	if (direction == 'W')
 		return grid[x][y - 1];
@@ -39,12 +39,12 @@ bool isReachable(bool grid[][41], char direction, int currentPositionX, int curr
 
 int main() {
 
-	sf::RenderWindow window(sf::VideoMode(gridSize*mapSize, gridSize*mapSize), "Pacman");
+	sf::RenderWindow window(sf::VideoMode(cellSize*mapSize, cellSize*mapSize), "Pacman");
 	window.setFramerateLimit(60);
 
 	int inputDelay = 20, autoMovement=10;
-	char movementDir='0'; 
-	float movementSpeed = gridSize;
+	char movementDir='0';  // initializing player as stationary
+	float movementSpeed = cellSize; // Moves a cell each time the player moves
 
 
 	//food
@@ -54,18 +54,18 @@ int main() {
 	if (!foodText.loadFromFile("Resources/Texture/food.png"))
 		std::cout << "failed to load food.png";
 	food.setTexture(foodText);
-	food.setScale(gridSize / food.getGlobalBounds().width , gridSize / food.getGlobalBounds().height);
+	food.setScale(cellSize / food.getGlobalBounds().width , cellSize / food.getGlobalBounds().height);
 	std::vector < sf::Sprite > foods;
 
 	
 	//Map
 	//const int mapSize = 40 + 1;
 	
-	bool obstacle[mapSize][mapSize] = { 1 };
+	bool notWall[mapSize][mapSize]; 
 
 	for (int i = 0; i < mapSize; i++)
 		for (int j = 0; j < mapSize; j++)
-			obstacle[i][j] = 1;
+			notWall[i][j] = 1; // Initializing the array as 1 meaning there is no wall
 	
 	std::vector<std::vector<sf::RectangleShape>> map;
 
@@ -76,17 +76,17 @@ int main() {
 		map[i].resize(mapSize, sf::RectangleShape());
 		for (int j = 0; j < mapSize; j++)
 		{
-			map[i][j].setSize(sf::Vector2f(gridSize,gridSize));
+			map[i][j].setSize(sf::Vector2f(cellSize,cellSize));
 			map[i][j].setFillColor(sf::Color::White);
 			/*map[i][j].setOutlineThickness(1.f);
 			map[i][j].setOutlineColor(sf::Color::Black);*/
-			map[i][j].setPosition(i * gridSize, j * gridSize);
+			map[i][j].setPosition(i * cellSize, j * cellSize);
 		}
 	}
 	std::ifstream mapInfo("Resources/Map/map.txt");
 	if (!mapInfo.is_open()) 
 		std::cout << "Error loading pacmanMap.txt" << '\n';
-	sf::Vector2f pacmanSpawn;
+	sf::Vector2f pacmanSpawn; // Stores the coordinate where pacman spawns at the start of the round
 	if(mapInfo.is_open())
 	{
 		std::string row;
@@ -95,27 +95,27 @@ int main() {
 		{
 			for (int i = 0; i < 40; i++)
 			{
-				if (row[i] == 'B')
+				if (row[i] == 'B') // if there is a wall
 				{
-					obstacle[pos][i] = 0;
+					notWall[pos][i] = 0;
 					map[pos][i].setFillColor(sf::Color::Black);
 				}
 					
 				else if (row[i] == 'P')
 				{
-					pacmanSpawn.x = pos * gridSize;
-					pacmanSpawn.y = i * gridSize;
+					pacmanSpawn.x = pos * cellSize; // change needed
+					pacmanSpawn.y = i * cellSize;
 				}
-				else if(rand()%6==1)
+				else if(rand()%6==1) // randomly spawns food
 				{
 					foods.push_back(food);
-					foods[foods.size()-1].setPosition(sf::Vector2f(pos*gridSize,i*gridSize));
+					foods[foods.size()-1].setPosition(sf::Vector2f(pos*cellSize,i*cellSize));
 				}
 			}
 			pos++;
 			row.clear();
 		}
-		std::cout << "grid " << obstacle[34][37]<<'\n';
+		std::cout << "grid " << notWall[34][37]<<'\n';
 
 		//while (!mapInfo.eof())
 		//{
@@ -150,7 +150,7 @@ int main() {
 		std::cout << "Failed to load playerRight.png";
 
 	player.setTexture(playerTextRight);
-	float playerSize = gridSize;
+	float playerSize = cellSize;
 	player.setScale(playerSize/1200,playerSize/1403 );
 	player.setPosition(pacmanSpawn);
 
@@ -164,20 +164,20 @@ int main() {
 	if (!BlinkyText.loadFromFile("Resources/Texture/blinky.png"))
 		std::cout << "failed to load blinky.png";
 	Blinky.setTexture(BlinkyText);
-	Blinky.setScale(gridSize/202.0,gridSize/201.0 );
+	Blinky.setScale(cellSize/202.0,cellSize/201.0 );
 	std::cout << "blinky= " << Blinky.getGlobalBounds().height << " " << Blinky.getGlobalBounds().width << '\n';
 
 	if (!ClydeText.loadFromFile("Resources/Texture/clyde.png"));
 		std::cout << "failed to load clyde.png";
 	Clyde.setTexture(ClydeText);
-	Clyde.setScale(gridSize/Clyde.getGlobalBounds().width,gridSize/Clyde.getGlobalBounds().height );
+	Clyde.setScale(cellSize/Clyde.getGlobalBounds().width,cellSize/Clyde.getGlobalBounds().height );
 	std::cout << "clyde = " << Clyde.getGlobalBounds().height << " " << Clyde.getGlobalBounds().width << '\n';
 	Clyde.setPosition(sf::Vector2f(window.getSize().x - Clyde.getGlobalBounds().width, 0.f));
 
 	if (!InkyText.loadFromFile("Resources/Texture/inky.png"));
 		std::cout << "failed to load inky.png";
 	Inky.setTexture(InkyText);
-	Inky.setScale(gridSize/Inky.getGlobalBounds().width ,gridSize/Inky.getGlobalBounds().height );
+	Inky.setScale(cellSize/Inky.getGlobalBounds().width ,cellSize/Inky.getGlobalBounds().height );
 	std::cout << "inky= " << Inky.getGlobalBounds().height << " " << Inky.getGlobalBounds().width << '\n';
 	Inky.setPosition(sf::Vector2f(0.f, window.getSize().y));
 	
@@ -203,7 +203,7 @@ int main() {
 		int posX = player.getPosition().x;
 		int posY = player.getPosition().y;
 
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && inputDelay <= 0 && posX > 0 && isReachable(obstacle, 'A', posX, posY ))
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && inputDelay <= 0 && posX > 0 && isReachable(notWall, 'A', posX, posY ))
 		{	
 			player.move(-1 * movementSpeed, 0.f);
 			movementDir = 'A';
@@ -212,7 +212,7 @@ int main() {
 			std::cout << "x = " << player.getPosition().x << " y = " << player.getPosition().y << '\n';
 		}
 
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && inputDelay <= 0 && posX < gridSize*mapSize - player.getGlobalBounds().width && isReachable(obstacle, 'D',posX,posY ))
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && inputDelay <= 0 && posX < cellSize*mapSize - player.getGlobalBounds().width && isReachable(notWall, 'D',posX,posY ))
 		{
 			player.move(movementSpeed, 0.f);
 			movementDir = 'D';
@@ -220,14 +220,14 @@ int main() {
 			inputDelay = autoMovement = 10;
 		}
 
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && inputDelay <= 0 && posY >0 && isReachable(obstacle, 'W', posX,posY))
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && inputDelay <= 0 && posY >0 && isReachable(notWall, 'W', posX,posY))
 		{
 			player.move(0.f, -1 * movementSpeed);
 			movementDir = 'W';
 			player.setTexture(playerTextUp);
 			inputDelay = autoMovement = 10;
 		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && inputDelay<=0 && posY + player.getGlobalBounds().height  < gridSize * mapSize && isReachable(obstacle, 'S', posX,posY)) {
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && inputDelay<=0 && posY + player.getGlobalBounds().height  < cellSize * mapSize && isReachable(notWall, 'S', posX,posY)) {
 			player.move(0.f, movementSpeed);
 			movementDir = 'S';
 			player.setTexture(playerTextDown);
@@ -237,26 +237,28 @@ int main() {
 		else if(autoMovement<=0)
 		{
 			autoMovement = 10;
-			if (movementDir == 'W' &&  posY > 0 && isReachable(obstacle, 'W', posX,posY))
+			if (movementDir == 'W' &&  posY > 0 && isReachable(notWall, 'W', posX,posY))
 				player.move(0.f, -1 * movementSpeed);
 
-			else if (movementDir == 'S' && posY + player.getGlobalBounds().height + 1 < gridSize*mapSize && isReachable(obstacle, 'S', posX,posY))
+			else if (movementDir == 'S' && posY + player.getGlobalBounds().height + 1 < cellSize*mapSize && isReachable(notWall, 'S', posX,posY))
 				player.move(0.f, movementSpeed);
 
-			else if (movementDir == 'A' && posX > 0 && isReachable(obstacle, 'A', posX,posY))
+			else if (movementDir == 'A' && posX > 0 && isReachable(notWall, 'A', posX,posY))
 				player.move(-1 * movementSpeed, 0.f);
 
-			else if (movementDir == 'D' && posX < gridSize * mapSize - player.getGlobalBounds().width && isReachable(obstacle, 'D', posX,posY))
+			else if (movementDir == 'D' && posX < cellSize * mapSize - player.getGlobalBounds().width && isReachable(notWall, 'D', posX,posY))
 				player.move(movementSpeed, 0.f);
 				
 		}
 
+		// Food update
 		for (int i = 0; i < foods.size(); i++)
 			if (foods[i].getGlobalBounds().intersects(player.getGlobalBounds()))
 				foods.erase(foods.begin() + i);
 
 		window.clear(sf::Color::Transparent);
 
+		// 
 
 		//draw
 		for (int i = 0; i < mapSize; i++)
